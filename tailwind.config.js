a53261e373281461ff29ea76b722c6a38242cc6b
Muh fajri aushaf <mfajriaushaf@gmail.com>
2025-02-10 12:17:38 +0700

    update section pengalaman

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['index.html'],
+  content: [
+    'index.html',
+    './*.html',
+    './**/*.html',
+  ],
   theme: {
     container:{
       center: true,
       padding:  '16px',
+    },
+    listStyleType:{
+      none:'none',
+      disc:'disc',
+      decimal: 'decimal',
+      square: 'square',
+      roman: 'upper-roman',
     },
     extend: {
       colors: {
         primary: '#14b8a6',
         secondary: '#64748b',
         dark: '#0f172a',
+        link: '#3b82f6',
       },
       screens:{
         '2xl': '1320px',
